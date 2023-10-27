b7f1773e6a619204d860f0c31a37f69538b921b7
Anthony Hernandez <tony91139@gmail.com>
2023-10-26 20:43:42 -0600

    docs: Fix JSDoc for sets types

--- types/sets.ts
+++ types/sets.ts
@@ -10,72 +10,72 @@ export type SetsResponse = {
   /**
    * The types of sets to include, e.g. 'expansion'
    */
-  type: string;
+  types: string[];
 };
 
 export type Set = {
   /**
-   * @example 0
+   * 0
    */
   card_count: number;
   /**
-   * @example "tlci"
+   * "tlci"
    */
   code: string;
   /**
-   * @example false
+   * false
    */
   digital: boolean;
   /**
-   * @example true
+   * true
    */
   foil_only: boolean;
   /**
-   * @example "https://svgs.scryfall.io/sets/default.svg?1698033600"
+   * "https://svgs.scryfall.io/sets/default.svg?1698033600"
    */
   icon_svg_uri: string;
   /**
-   * @example "07ee7ac3-059b-4de5-924a-2bd1b7910c8b"
+   * "07ee7ac3-059b-4de5-924a-2bd1b7910c8b"
    */
   id: string;
   /**
-   * @example "Lost Caverns of Ixalan Tokens"
+   * "Lost Caverns of Ixalan Tokens"
    */
   name: string;
   /**
-   * @example true
+   * true
    */
   nonfoil_only: boolean;
   /**
-   * @example "set"
+   * "set"
    */
   object: string;
   /**
-   * @example "lci"
+   * "lci"
    */
   parent_set_code?: string;
   /**
-   * @example "2023-11-17"
+   * "2023-11-17"
    */
   released_at: string;
   /**
-   * @example "https://scryfall.com/sets/tlci"
+   * "https://scryfall.com/sets/tlci"
    */
   scryfall_uri: string;
   /**
-   * @example "https://api.scryfall.com/cards/search?include_extras=true&include_variations=true&order=set&q=e%3Atlci&unique=prints"
+   * "https://api.scryfall.com/cards/search?include_extras=true&include_variations=true&order=set&q=e%3Atlci&unique=prints"
    */
   search_uri: string;
   /**
-   * @example "token"
+   * "token"
    */
   set_type: string;
   /**
-   * @example 23337
+   * 23337
    */
   tcgplayer_id?: number;
   /**
-   * @example "https://api.scryfall.com/sets/07ee7ac3-059b-4de5-924a-2bd1b7910c8b"
+   * "https://api.scryfall.com/sets/07ee7ac3-059b-4de5-924a-2bd1b7910c8b"
    */
   uri: string;
 };
