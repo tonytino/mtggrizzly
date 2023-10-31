cd56839c862db58afb6ed90e086deb651005d888
Anthony Hernandez <tony91139@gmail.com>
2023-10-31 15:12:06 -0600

    style: Improve appearance of set page

--- src/app/sets/[set]/page.tsx
+++ src/app/sets/[set]/page.tsx
@@ -56,23 +56,25 @@ async function SetPage({
       </h1>
 
       <section className='m-auto grid h-min w-full grid-cols-1 place-content-center gap-8 md:grid-cols-2 lg:grid-cols-3'>
-        {cards.map(({ image_uris = {}, name }) => {
-          const { small } = image_uris ?? {};
+        {cards.map(({ image_uris = {}, name }, index) => {
+          const { border_crop } = image_uris ?? {};
           return (
             <div
-              className='flex h-44 w-72 cursor-pointer flex-col items-center justify-between rounded border-2 p-4 text-center shadow-md hover:border-teal-400'
+              className='flex cursor-pointer flex-col items-center justify-between text-center'
               key={name}
             >
-              <div className='relative mx-auto my-2 h-14 w-14 flex-col items-center justify-center'>
+              <div className='relative mx-auto my-2 flex-col items-center justify-center rounded-lg border-2 border-black bg-black shadow-md hover:border-teal-400'>
                 <Image
+                  className='rounded-lg border-4 border-black'
                   alt={name}
-                  src={small}
-                  fill
-                  sizes='100%'
+                  src={border_crop}
+                  height={680}
+                  width={488}
+                  priority={index < 4}
                 />
               </div>
 
-              <p className='my-auto text-lg font-medium text-slate-800'>
+              <p className='my-auto text-lg font-medium text-slate-800 dark:text-slate-50'>
                 {name}
               </p>
             </div>
