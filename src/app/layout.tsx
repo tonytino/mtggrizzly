7675cbc5a0d9d4931282c76374f2328ba4ff2433
Anthony Hernandez <tony91139@gmail.com>
2023-11-03 02:09:14 -0600

    feat: Make website name link to home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Analytics } from '@vercel/analytics/react';
 import { Inter } from 'next/font/google';
+import Link from 'next/link';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -23,7 +24,9 @@ export default function RootLayout({
       <body className={inter.className}>
         <main className='flex min-h-screen w-full flex-col items-center justify-between px-4 pb-10'>
           <div className='fixed left-0 top-0 z-50 h-16 w-full bg-slate-800 shadow-lg	'>
-            <h1 className='p-4 pl-8 text-3xl font-bold'>MTG Grizzly</h1>
+            <h1 className='p-4 pl-8 text-3xl font-bold'>
+              <Link href='/'>MTG Grizzly</Link>
+            </h1>
           </div>
 
           <div className='left-0 mt-20 flex h-full w-full flex-col items-center justify-start gap-4 overflow-y-auto from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto'>
