fe9ae3ccdc0be6da19825eb0fd6f80e4d38c2528
Anthony Hernandez <tony91139@gmail.com>
2023-11-07 17:14:00 -0700

    feat: Update set page to use new payload data
    Resolves #6

--- src/app/sets/[set]/page.tsx
+++ src/app/sets/[set]/page.tsx
@@ -1,24 +1,7 @@
 import { headers } from 'next/headers';
 import * as React from 'react';
 import { Card } from '@/components';
-import type { DraftSet, SetsResponse } from '@/types';
-
-/**
- * @todo https://github.com/tonytino/mtggrizzly/issues/6
- * Move this request to /api/set/[set] and include in the response
- */
-async function getSets(): Promise<SetsResponse> {
-  const reqHeaders = headers();
-  const host = reqHeaders.get('host') ?? 'localhost:3000';
-  const protocol = host.includes('localhost') ? 'http' : 'https';
-  const res = await fetch(`${protocol}://${host}/api/sets`);
-
-  if (!res.ok) {
-    throw new Error('Failed to fetch sets data');
-  }
-
-  return res.json();
-}
+import type { DraftSet } from '@/types';
 
 async function getSet(set: string): Promise<DraftSet> {
   const reqHeaders = headers();
@@ -43,15 +26,13 @@ async function SetPage({
   // searchParams: { name: string };
 }) {
   const { set } = params;
-  const { sets } = await getSets();
-  const { cards } = await getSet(set);
-
-  const setName = sets.find(({ code }) => code === set)?.name ?? 'Weird...';
+  const { cards, set: setDetails } = await getSet(set);
+  const { name, code } = setDetails;
 
   return (
     <React.Fragment>
       <h1 className='text-3xl font-bold'>
-        {setName} ({set?.toUpperCase()})
+        {name} ({code.toUpperCase()})
       </h1>
 
       <section className='m-auto grid h-min w-full grid-cols-1 place-content-center gap-8 md:grid-cols-2 lg:grid-cols-3'>
