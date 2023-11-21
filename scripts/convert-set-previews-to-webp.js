ebfcafc9c2c864dd6ff3051b735e723eff9d2798
Anthony Hernandez <tony91139@gmail.com>
2023-11-21 12:50:20 -0700

    refactor: Reduce quality of webp images

--- scripts/convert-set-previews-to-webp.js
+++ scripts/convert-set-previews-to-webp.js
@@ -1,7 +1,8 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
 /**
- * BLAH BLAH
+ * Converts jpg set preview images to webp in both 320w and 384w sizes using
+ * cwebp: https://developers.google.com/speed/webp/docs/cwebp
  */
 
 const { execSync } = require('child_process');
@@ -9,18 +10,14 @@ const { getSetsJSON } = require('./utils');
 
 const sets = getSetsJSON();
 
-/**
- * BLAH BLAH
- */
-
 sets.forEach((set, index) => {
   console.log(`[${index + 1}/${sets.length}] Processing ${set.name}.`);
 
   execSync(
-    `cwebp ${__dirname}/../public/set-previews/${set.code}.jpg -o ${__dirname}/../public/set-previews/320/${set.code}.webp -af -q 100 -hint picture -jpeg_like -resize 320 0`
+    `cwebp ${__dirname}/../public/set-previews/${set.code}.jpg -o ${__dirname}/../public/set-previews/320/${set.code}.webp -af -hint picture -resize 320 0`
   );
 
   execSync(
-    `cwebp ${__dirname}/../public/set-previews/${set.code}.jpg -o ${__dirname}/../public/set-previews/384/${set.code}.webp -af -q 100 -hint picture -jpeg_like -resize 384 0`
+    `cwebp ${__dirname}/../public/set-previews/${set.code}.jpg -o ${__dirname}/../public/set-previews/384/${set.code}.webp -af -hint picture -resize 384 0`
   );
 });
