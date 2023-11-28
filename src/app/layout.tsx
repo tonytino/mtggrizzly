ae12e8fc0ba3f08613b50077d1f1a9706415ce8e
Anthony Hernandez <tony91139@gmail.com>
2023-11-28 14:06:12 -0700

    feat: Create proper header and footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,20 +21,63 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={inter.className}>
+        <header>
+          <div className='fixed left-0 right-0 top-0 z-50 w-screen bg-white shadow-sm dark:bg-slate-800'>
+            <nav className='mx-auto flex h-16 w-full items-baseline justify-between p-4 xl:max-w-screen-xl'>
+              <div>
+                <h1 className='text-3xl font-bold text-slate-800 dark:text-slate-100'>
+                  <Link href='/'>MTG Grizzly</Link>
+                </h1>
+              </div>
+
+              <div className='flex gap-6 lg:gap-12'>
+                <h2 className='text-xl font-bold text-slate-800 dark:text-slate-100'>
+                  <Link href='/sets'>Sets</Link>
+                </h2>
+
+                <h2 className='text-xl font-bold text-slate-800 dark:text-slate-100'>
+                  <Link href='/about'>About</Link>
+                </h2>
+              </div>
+            </nav>
+          </div>
+        </header>
+
         <main
           className={`flex min-h-screen flex-col items-center justify-between pb-10 ${WIDTH_CONSTRAINTS}`}
         >
-          <div className='fixed left-0 top-0 z-50 h-16 w-full bg-white shadow-sm dark:bg-slate-800'>
-            <h1 className={`py-4 text-3xl font-bold ${WIDTH_CONSTRAINTS}`}>
-              <Link href='/'>MTG Grizzly</Link>
-            </h1>
-          </div>
-
-          <div className='left-0 mt-20 flex h-full w-full flex-col items-center justify-start gap-4'>
+          <div className='left-0 mt-16 flex h-full w-full flex-col items-center justify-start gap-4'>
             {children}
           </div>
         </main>
 
+        <footer className='flex h-60 w-screen flex-col items-center justify-end gap-4 bg-gradient-to-b from-white from-0% to-slate-300 py-16 text-center dark:from-slate-800 dark:to-slate-950'>
+          <p className='text-lg text-slate-600 dark:text-slate-100'>
+            Thanks for using MTG Grizzly!
+          </p>
+
+          <p className='text-md text-end leading-8 text-slate-600 dark:text-slate-100'>
+            Special thanks to&nbsp;
+            <a
+              className='text-lg font-semibold text-slate-600 dark:text-slate-500'
+              href='https://scryfall.com/'
+              rel='noreferrer'
+              target='_blank'
+            >
+              Scryfall
+            </a>
+            &nbsp;and&nbsp;
+            <a
+              className='text-lg font-semibold text-slate-600 dark:text-slate-500'
+              href='https://magic.wizards.com/'
+              rel='noreferrer'
+              target='_blank'
+            >
+              Wizards of the Coast
+            </a>
+            &nbsp;
+          </p>
+        </footer>
         <Analytics />
       </body>
     </html>
