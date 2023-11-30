d4adfc7bae1248fefe86760ec6db811eda15e804
Anthony Hernandez <tony91139@gmail.com>
2023-11-30 16:46:32 -0700

    feat: Add <ScrollToTopIcon /> component

---
+++ src/app/ScrollToTopIcon.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import * as Tooltip from '@radix-ui/react-tooltip';
+import { ThickArrowUpIcon } from '@radix-ui/react-icons';
+
+export default function ScrollToTopIcon() {
+  return (
+    <Tooltip.Root>
+      <Tooltip.Trigger>
+        <ThickArrowUpIcon
+          className='mt-[3px] cursor-pointer text-sky-800 hover:text-sky-600 dark:text-sky-400 dark:hover:text-sky-300'
+          height='36px'
+          onClick={() =>
+            window.scrollTo({
+              behavior: 'smooth',
+              left: 0,
+              top: 0,
+            })
+          }
+          width='36px'
+        />
+      </Tooltip.Trigger>
+
+      <Tooltip.Content className='mb-2 rounded bg-sky-800 p-2 text-sm text-slate-100 shadow-sm dark:bg-slate-50 dark:text-sky-800'>
+        Scroll to top
+      </Tooltip.Content>
+    </Tooltip.Root>
+  );
+}
