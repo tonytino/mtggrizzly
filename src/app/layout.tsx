0f2ba923b6fd14ea5a4ef4152c6e2dc3a2f6d525
Anthony Hernandez <tony91139@gmail.com>
2023-11-30 16:54:24 -0700

    feat: Implement <Footer /> component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import './globals.css';
 import type { Metadata } from 'next';
-import Link from 'next/link';
 import { Analytics } from '@vercel/analytics/react';
 import { Inter } from 'next/font/google';
+import Footer from './Footer';
+import Header from './Header';
 import Providers from './Providers';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -19,46 +20,21 @@ export default function RootLayout({
 }) {
   return (
     <Providers>
-    <html lang='en'>
-      <body className={inter.className}>
+      <html lang='en'>
+        <body className={inter.className}>
           <Header />
 
-        <main className='mx-auto flex min-h-screen w-full flex-col items-center justify-between px-4 pb-10'>
-          <div className='left-0 mt-16 flex h-full w-full flex-col items-center justify-start gap-4 xl:max-w-screen-xl'>
-            {children}
-          </div>
-        </main>
+          <main className='mx-auto flex min-h-screen w-full flex-col items-center justify-between px-4 pb-10'>
+            <div className='left-0 mt-16 flex h-full w-full flex-col items-center justify-start gap-4 xl:max-w-screen-xl'>
+              {children}
+            </div>
+          </main>
 
-        <footer className='flex h-96 w-screen flex-col items-center justify-end gap-4 bg-slate-100 px-8 py-16 text-center dark:bg-slate-900'>
-          <p className='text-md text-slate-600 dark:text-slate-100 md:text-lg'>
-            Thanks for using MTG Grizzly!
-          </p>
+          <Footer />
 
-          <p className='md:text-md text-center text-sm leading-8 text-slate-600 dark:text-slate-100'>
-            Special thanks to&nbsp;
-            <a
-              className='text-md font-semibold text-slate-600 dark:text-slate-500 md:text-lg'
-              href='https://scryfall.com/'
-              rel='noreferrer'
-              target='_blank'
-            >
-              Scryfall
-            </a>
-            &nbsp;and&nbsp;
-            <a
-              className='text-md font-semibold text-slate-600 dark:text-slate-500 md:text-lg'
-              href='https://magic.wizards.com/'
-              rel='noreferrer'
-              target='_blank'
-            >
-              Wizards of the Coast
-            </a>
-            &nbsp; for making this project possible.
-          </p>
-        </footer>
-        <Analytics />
-      </body>
-    </html>
+          <Analytics />
+        </body>
+      </html>
     </Providers>
   );
 }
