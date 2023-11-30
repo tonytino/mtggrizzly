bffb9624ff8acd64531aeb5624467c9bb36a2aaf
Anthony Hernandez <tony91139@gmail.com>
2023-11-30 16:18:31 -0700

    feat: Add <Header /> component

---
+++ src/app/Header.tsx
@@ -0,0 +1,54 @@
+import Link from 'next/link';
+
+/**
+ * Renders all aspects of the website's header
+ */
+export default function Header() {
+  return (
+    <header>
+      <div className='fixed left-0 right-0 top-0 z-50 w-screen bg-white shadow-sm dark:bg-slate-800'>
+        <nav className='mx-auto flex h-16 w-full items-baseline justify-between p-4 md:py-4 xl:max-w-screen-xl'>
+          <Link
+            className='text-3xl font-bold text-sky-800 dark:text-slate-100'
+            href='/'
+          >
+            MTG Grizzly
+          </Link>
+
+          <div className='flex gap-6 lg:gap-12'>
+            <PrimaryNavLink href='/sets'>Sets</PrimaryNavLink>
+
+            <PrimaryNavLink href='/about'>About</PrimaryNavLink>
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
+}
+
+type PrimaryNavLinkProps = {
+  /**
+   * Text to render for the link
+   */
+  children: string;
+  /**
+   * Destination to pass to href property
+   */
+  href: string;
+};
+
+/**
+ * Renders primary nav links with consistent styling
+ */
+function PrimaryNavLink(props: PrimaryNavLinkProps) {
+  const { children, href } = props;
+
+  return (
+    <Link
+      className='text-xl font-bold text-sky-800 hover:text-sky-600 dark:text-slate-100 dark:hover:text-sky-300'
+      href={href}
+    >
+      {children}
+    </Link>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,27 +19,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={inter.className}>
-        <header>
-          <div className='fixed left-0 right-0 top-0 z-50 w-screen bg-white shadow-sm dark:bg-slate-800'>
-            <nav className='mx-auto flex h-16 w-full items-baseline justify-between p-4 xl:max-w-screen-xl'>
-              <div>
-                <h1 className='text-3xl font-bold text-slate-800 dark:text-slate-100'>
-                  <Link href='/'>MTG Grizzly</Link>
-                </h1>
-              </div>
-
-              <div className='flex gap-6 lg:gap-12'>
-                <h2 className='text-xl font-bold text-slate-800 dark:text-slate-100'>
-                  <Link href='/sets'>Sets</Link>
-                </h2>
-
-                <h2 className='text-xl font-bold text-slate-800 dark:text-slate-100'>
-                  <Link href='/about'>About</Link>
-                </h2>
-              </div>
-            </nav>
-          </div>
-        </header>
+          <Header />
 
         <main className='mx-auto flex min-h-screen w-full flex-col items-center justify-between px-4 pb-10'>
           <div className='left-0 mt-16 flex h-full w-full flex-col items-center justify-start gap-4 xl:max-w-screen-xl'>
