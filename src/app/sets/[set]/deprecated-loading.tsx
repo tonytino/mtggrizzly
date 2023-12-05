e77fdd6b51bf2ba9b6439b285fc3eab0806e01a5
Anthony Hernandez <tony91139@gmail.com>
2023-12-05 16:34:27 -0700

    chore: Disable (temp) the loading state for set page

--- src/app/sets/[set]/deprecated-loading.tsx
+++ src/app/sets/[set]/deprecated-loading.tsx
@@ -1,3 +1,14 @@
+/**
+ * I've renamed this from loading.tsx for now as this causes the lighthouse performance metric to drop from 80 to 64, which appears to be largely driven by the LCP impact of the card images being dynamically rendered.
+ *
+ * Here's the warning from lighthouse about this:
+ *
+ * "If the LCP element is dynamically added to the page, you should preload the image in order to improve LCP. [Learn more about preloading LCP elements."](https://web.dev/articles/optimize-lcp?utm_source=lighthouse&utm_medium=devtools#optimize-when-the-resource-is-discovered)
+ *
+ * I'll rename this back once the changes necessary to prevent the lighthouse
+ * hit are ready.
+ */
+
 import * as React from 'react';
 
 export default function Loading() {
