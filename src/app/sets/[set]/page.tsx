dbfa95aca4d3c947e356932c52224da81ea98073
Anthony Hernandez <tony91139@gmail.com>
2023-12-05 11:12:15 -0700

    chore: Remove set code from set page

--- src/app/sets/[set]/page.tsx
+++ src/app/sets/[set]/page.tsx
@@ -38,12 +38,12 @@ async function SetPage({
 }) {
   const { set } = params;
   const { cards, set: setDetails } = await getSet(set);
-  const { name, code } = setDetails;
+  const { name } = setDetails;
 
   return (
     <React.Fragment>
       <h1 className='mb-4 text-3xl font-bold text-sky-900 dark:text-slate-100 xl:mb-8'>
-        {name} ({code.toUpperCase()})
+        {name}
       </h1>
 
       <div className='m-auto grid h-min w-full grid-cols-1 place-content-center gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-8'>
