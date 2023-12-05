cf2b14653d93d004a41316690bfd3ee8078a0a4b
Anthony Hernandez <tony91139@gmail.com>
2023-12-04 20:30:30 -0700

    style: Tighten set page cards grid

--- src/app/sets/[set]/page.tsx
+++ src/app/sets/[set]/page.tsx
@@ -46,7 +46,7 @@ async function SetPage({
         {name} ({code.toUpperCase()})
       </h1>
 
-      <div className='m-auto grid h-min w-full grid-cols-1 place-content-center gap-8 md:grid-cols-2 lg:grid-cols-3'>
+      <div className='m-auto grid h-min w-full grid-cols-1 place-content-center gap-4 md:grid-cols-2 lg:grid-cols-3 xl:gap-8'>
         {cards.map((card, index) => {
           return (
             <Card
