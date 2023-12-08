1536522c87c73d24ac6f554712a651f99790142d
Anthony Hernandez <tony91139@gmail.com>
2023-12-08 15:32:50 -0700

    test(<Set />): Update tests to reflect recent updates

--- src/components/Set/Set.test.tsx
+++ src/components/Set/Set.test.tsx
@@ -1,78 +1,70 @@
-import { React, render, screen, userEvent } from 'test-utils';
+import { React, render, screen } from 'test-utils';
 import type { Set as SetType } from '@/types';
 import { Set } from './Set';
 import SampleSet from './Set.sample.json';
-import { CLASSES_FOR_MAGIC_COLORS } from './constants';
-
-const pushMock = jest.fn();
-
-jest.mock('next/navigation', () => {
-  return {
-    useRouter: () => {
-      return {
-        push: pushMock,
-      };
-    },
-  };
-});
 
 const set = SampleSet as SetType;
 
 describe('<Set />', () => {
   describe('features', () => {
-    test('renders the set image alongside the set name', () => {
-      render(
-        <Set
-          index={0}
-          set={set}
-        />
-      );
+    test('renders the set name', () => {
+      render(<Set set={set} />);
 
-      const setIcon = screen.getByAltText(`${set.name} Set Icon`);
-      const setPreviewImage = screen.getByRole('button');
       const setName = screen.getByText(set.name);
-
-      expect(setIcon).toBeInTheDocument();
-      expect(setIcon).toHaveAttribute('src', set.icon_svg_uri);
-
-      expect(setPreviewImage).toBeInTheDocument();
-      expect(setPreviewImage).toHaveStyle(
-        `background-image: url(${set.preview_art})`
-      );
 
       expect(setName).toBeInTheDocument();
     });
 
-    describe('when clicked', () => {
-      test('navigates you to the set page', async () => {
-        render(
-          <Set
-            index={0}
-            set={set}
-          />
-        );
+    test('renders a link to the set page', async () => {
+      render(<Set set={set} />);
 
-        const setPreviewImage = screen.getByRole('button');
+      const setPageLink = screen.getByRole('link');
 
-        await userEvent.click(setPreviewImage);
-
-        expect(pushMock).toHaveBeenCalledWith(`/sets/${set.code}`);
-      });
+      expect(setPageLink).toBeInTheDocument();
+      expect(setPageLink).toHaveAttribute('href', `/sets/${set.code}`);
     });
-  });
 
-  describe('prop: index', () => {
-    test('renders with a fallback background color', () => {
-      render(
-        <Set
-          index={0}
-          set={set}
-        />
+    test('renders the set preview card art', () => {
+      render(<Set set={set} />);
+
+      const setPreviewImage = screen.getByAltText(
+        `${set.name}: Preview Card Art`
       );
 
-      const setPreviewImage = screen.getByRole('button');
+      expect(setPreviewImage).toBeInTheDocument();
+      expect(setPreviewImage).toHaveAttribute(
+        'src',
+        `/sets/${set.code}/preview/576.webp`
+      );
+    });
 
-      expect(setPreviewImage).toHaveClass(CLASSES_FOR_MAGIC_COLORS[0]);
+    test('renders the set icon', () => {
+      render(<Set set={set} />);
+
+      const setIcon = screen.getByAltText(`${set.name}: Set Icon`);
+
+      expect(setIcon).toBeInTheDocument();
+      expect(setIcon).toHaveAttribute('src', set.icon_svg_uri);
+    });
+
+    test('renders the name of the artist for the preview art card', () => {
+      render(<Set set={set} />);
+
+      expect(screen.getByText(set.preview_card.artist)).toBeInTheDocument();
+    });
+
+    test('renders the WOTC trademark and copyright', () => {
+      render(<Set set={set} />);
+
+      expect(
+        screen.getByText('™ & © Wizards of the Coast')
+      ).toBeInTheDocument();
+    });
+
+    test('renders the pencil icon', () => {
+      render(<Set set={set} />);
+
+      expect(screen.getByTestId('pencil-icon')).toBeInTheDocument();
     });
   });
 });
