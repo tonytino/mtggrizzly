e33f3e64fe602ca170498cbd834ce7c60ae8ac2e
Anthony Hernandez <tony91139@gmail.com>
2023-12-09 18:03:51 -0700

    chore(cicd): Add jest configs related to coverage

--- jest.config.js
+++ jest.config.js
@@ -1,23 +1,35 @@
-// const nextJest = require("next/jest");
-// const createJestConfig = nextJest({
-//   dir: "./",
-// });
-// const customJestConfig = {
-//   moduleDirectories: ["node_modules", "<rootDir>/"],
-//   testEnvironment: "jest-environment-jsdom",
-// };
-// module.exports = createJestConfig(customJestConfig);
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const nextJest = require('next/jest');
 
 // Providing the path to your Next.js app which will enable loading next.config.js and .env files
 const createJestConfig = nextJest({ dir: './' });
 
-// Any custom config you want to pass to Jest
 const customJestConfig = {
+  /**
+   * Coverage-related configs
+   */
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coveragePathIgnorePatterns: [
+    './*/index.ts',
+    './node_modules',
+    './scripts',
+    './types',
+    './utils',
+  ],
+  coverageProvider: 'v8',
+  coverageThreshold: {
+    global: {
+      branches: 10,
+      functions: 10,
+      lines: 10,
+      statements: 10,
+    },
+  },
+
   moduleDirectories: ['node_modules', 'utils', __dirname],
+
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+
   testEnvironment: 'jsdom',
 };
 
