1df0f111d5a48c72602d0a0f5fa4aae718af4383
Anthony Hernandez <tony91139@gmail.com>
2023-12-11 18:40:18 -0700

    feat(set page): Add ability to search a sets cards
    This is a very basic implementation of this feature and it will likely
    look very different once we start to add more filtering/searching
    options.

---
+++ src/app/sets/[set]/SetPageCards.tsx
@@ -0,0 +1,87 @@
+'use client';
+
+import * as React from 'react';
+import { Card } from '@/components';
+import type { Card as CardType } from '@/types';
+
+type SetPageCardsType = {
+  /**
+   * The cards for the given set
+   */
+  cards: CardType[];
+};
+
+/**
+ * The Cards for a given Set
+ */
+function SetPageCards(props: SetPageCardsType) {
+  const { cards } = props;
+  const [query, setQuery] = React.useState('');
+
+  const filteredCards = cards.filter((card) => {
+    const hasMultipleFaces = Boolean(card?.card_faces?.length);
+    const aspectsToCheck = hasMultipleFaces
+      ? [
+          card.card_faces[0].oracle_text,
+          card.card_faces[0].name,
+          card.card_faces[0].type_line,
+          card.card_faces[1].oracle_text,
+          card.card_faces[1].name,
+          card.card_faces[1].type_line,
+        ]
+      : [card.oracle_text, card.name, card.type_line];
+
+    return aspectsToCheck.some((aspect) =>
+      aspect.toLowerCase().includes(query.toLowerCase())
+    );
+  });
+
+  return (
+    <React.Fragment>
+      <div className='relative mx-auto my-4 flex min-w-full flex-col items-start gap-2 text-slate-500 focus-within:text-sky-800 dark:text-slate-100 dark:focus-within:text-slate-100 md:min-w-[22rem]'>
+        <label
+          className='w-full font-bold'
+          htmlFor='card-query-input'
+        >
+          Search
+          {Boolean(query.length) && (
+            <span className='float-right'>Hits: {filteredCards.length}</span>
+          )}
+        </label>
+
+        <input
+          className='w-full rounded-lg border-2 border-slate-200 p-2 px-4 dark:border-transparent dark:text-sky-700'
+          name='card-query-input'
+          onChange={(event) => setQuery(event.target.value)}
+          placeholder='Storm...'
+          type='text'
+          value={query}
+        />
+
+        {Boolean(query.length) && (
+          <button
+            className='absolute bottom-[calc(0.5rem+2px)] right-4 dark:text-sky-700'
+            onClick={() => setQuery('')}
+            type='button'
+          >
+            Clear
+          </button>
+        )}
+      </div>
+
+      <div className='m-auto grid h-min w-full grid-cols-1 place-content-center gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 xl:gap-8'>
+        {filteredCards.map((card, index) => {
+          return (
+            <Card
+              card={card}
+              isPriority={index < 9}
+              key={card.name}
+            />
+          );
+        })}
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default SetPageCards;
--- src/app/sets/[set]/page.tsx
+++ src/app/sets/[set]/page.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
-import { Card } from '@/components';
 import type { Card as CardType, DraftSet } from '@/types';
 import sets from '@/src/app/api/sets/sets.json';
+import SetPageCards from './SetPageCards';
 
 /**
  * [Search Syntax](https://scryfall.com/docs/syntax#sets)
@@ -90,17 +90,7 @@ async function SetPage({
         {name}
       </h1>
 
-      <div className='m-auto grid h-min w-full grid-cols-1 place-content-center gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 xl:gap-8'>
-        {cards.map((card, index) => {
-          return (
-            <Card
-              card={card}
-              isPriority={index < 9}
-              key={card.name}
-            />
-          );
-        })}
-      </div>
+      <SetPageCards cards={cards} />
     </React.Fragment>
   );
 }
