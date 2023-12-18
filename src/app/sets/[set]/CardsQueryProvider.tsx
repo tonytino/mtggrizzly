8b4802f46bab738fc1376aa29b931b09197d079e
Anthony Hernandez <tony91139@gmail.com>
2023-12-18 13:48:06 -0700

    feat: Create <CardsQueryProvider />

---
+++ src/app/sets/[set]/CardsQueryProvider.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import * as React from 'react';
+import CardsQueryContext from './CardsQueryContext';
+
+/**
+ * This component exposes the CardsQueryContext to its children and manages the
+ * state of the context
+ */
+function CardsQueryProvider(props: { children: React.ReactNode }) {
+  const { children } = props;
+
+  const [queries, setQueries] = React.useState({
+    permittedColors: [],
+    searchText: '',
+  });
+
+  return (
+    <CardsQueryContext.Provider value={{ ...queries, setQueries }}>
+      {children}
+    </CardsQueryContext.Provider>
+  );
+}
+
+export default CardsQueryProvider;
