ac8b12fad64eea98a5bcaae6a61b66010cb48fa3
Anthony Hernandez <tony91139@gmail.com>
2023-12-24 10:54:14 -0700

    feat(<SetPageCards />): Add logic for color filtering

--- src/app/sets/[set]/SetPageCards.tsx
+++ src/app/sets/[set]/SetPageCards.tsx
@@ -17,27 +17,74 @@ type SetPageCardsType = {
  */
 function SetPageCards(props: SetPageCardsType) {
   const { cards } = props;
-  const { searchText } = React.useContext(CardsQueryContext);
+  const { permittedColors, searchText } = React.useContext(CardsQueryContext);
 
-  const isSearchTextPresent = Boolean(searchText);
+  const isSearchTextPresent = Boolean(searchText?.length);
+  const arePermittedColorsPresent = Boolean(permittedColors?.length);
 
-  const filteredCards = isSearchTextPresent
+  const areFiltersPresent = [
+    isSearchTextPresent,
+    arePermittedColorsPresent,
+  ].some(Boolean);
+
+  const filteredCards = areFiltersPresent
     ? cards.filter((card) => {
         const hasMultipleFaces = Boolean(card?.card_faces?.length);
-        const aspectsToCheck = hasMultipleFaces
-          ? [
-              card.card_faces[0].oracle_text,
-              card.card_faces[0].name,
-              card.card_faces[0].type_line,
-              card.card_faces[1].oracle_text,
-              card.card_faces[1].name,
-              card.card_faces[1].type_line,
-            ]
-          : [card.oracle_text, card.name, card.type_line];
 
-        return aspectsToCheck.some((aspect) =>
-          aspect.toLowerCase().includes(searchText.toLowerCase())
-        );
+        /**
+         * Check if the card passes the search text
+         */
+
+        if (isSearchTextPresent) {
+          const searchTextAspects = hasMultipleFaces
+            ? [
+                card.card_faces[0].oracle_text,
+                card.card_faces[0].name,
+                card.card_faces[0].type_line,
+                card.card_faces[1].oracle_text,
+                card.card_faces[1].name,
+                card.card_faces[1].type_line,
+              ]
+            : [card.oracle_text, card.name, card.type_line];
+
+          const matchesSearchText = searchTextAspects.some((aspect) =>
+            aspect.toLowerCase().includes(searchText.toLowerCase())
+          );
+
+          if (!matchesSearchText) {
+            return false;
+          }
+        }
+
+        /**
+         * Check if the card passes the permitted colors
+         */
+
+        if (arePermittedColorsPresent) {
+          console.log('yes');
+          const cardColors = hasMultipleFaces
+            ? [...card.card_faces[0].colors, ...card.card_faces[1].colors]
+            : card.colors;
+
+          const someCardColorsIncludedInQuery = permittedColors.some(
+            (permittedColor) => {
+              if (permittedColor === 'Colorless') {
+                // If the card is colorless, and we want colorless cards
+                // included, it'll have an empty array for its colors property
+                return cardColors.length === 0;
+              } else {
+                return cardColors.includes(permittedColor);
+              }
+            }
+          );
+
+          if (!someCardColorsIncludedInQuery) {
+            return false;
+          }
+        }
+
+        // If we've made it this far, the card did not fail any of the checks
+        return true;
       })
     : cards;
 
