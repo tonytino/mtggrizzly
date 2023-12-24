776c32d7792b5c6a71edf40e6c34ae1fc3b8c688
Anthony Hernandez <tony91139@gmail.com>
2023-12-24 12:54:29 -0700

    refactor(<SetPageCards />): Filter via mana_cost
    Previously, the `colors` property was being used to filter cards via color. However, this led to issues with cards that lacked the `colors` property on their `card_faces` enumerables, e.g. adventure cards like Kellan, Daring Traveler: https://api.scryfall.com/cards/01739030-c280-492b-a5c9-b3e9f6debc6d.
    
    Instead, we'll use the `mana_cost` property, which should be much more reliable.

--- src/app/sets/[set]/SetPageCards.tsx
+++ src/app/sets/[set]/SetPageCards.tsx
@@ -1,9 +1,12 @@
 'use client';
 
 import * as React from 'react';
+import warning from 'warning';
 import { Card } from '@/components';
 import type { Card as CardType } from '@/types';
 import CardsQueryContext from './CardsQueryContext';
+
+const colorsRegex = new RegExp('[WUBRG]');
 
 type SetPageCardsType = {
   /**
@@ -29,62 +32,81 @@ function SetPageCards(props: SetPageCardsType) {
 
   const filteredCards = areFiltersPresent
     ? cards.filter((card) => {
-        const hasMultipleFaces = Boolean(card?.card_faces?.length);
+        try {
+          const hasMultipleFaces = Boolean(card?.card_faces?.length);
 
-        /**
-         * Check if the card passes the search text
-         */
+          /**
+           * Check if the card passes the search text
+           */
 
-        if (isSearchTextPresent) {
-          const searchTextAspects = hasMultipleFaces
-            ? [
-                card.card_faces[0].oracle_text,
-                card.card_faces[0].name,
-                card.card_faces[0].type_line,
-                card.card_faces[1].oracle_text,
-                card.card_faces[1].name,
-                card.card_faces[1].type_line,
-              ]
-            : [card.oracle_text, card.name, card.type_line];
+          if (isSearchTextPresent) {
+            const searchTextAspects = hasMultipleFaces
+              ? [
+                  card.card_faces[0].oracle_text,
+                  card.card_faces[0].name,
+                  card.card_faces[0].type_line,
+                  card.card_faces[1].oracle_text,
+                  card.card_faces[1].name,
+                  card.card_faces[1].type_line,
+                ]
+              : [card.oracle_text, card.name, card.type_line];
 
-          const matchesSearchText = searchTextAspects.some((aspect) =>
-            aspect.toLowerCase().includes(searchText.toLowerCase())
-          );
+            const matchesSearchText = searchTextAspects.some((aspect) =>
+              aspect.toLowerCase().includes(searchText.toLowerCase())
+            );
 
-          if (!matchesSearchText) {
-            return false;
-          }
-        }
-
-        /**
-         * Check if the card passes the permitted colors
-         */
-
-        if (arePermittedColorsPresent) {
-          console.log('yes');
-          const cardColors = hasMultipleFaces
-            ? [...card.card_faces[0].colors, ...card.card_faces[1].colors]
-            : card.colors;
-
-          const someCardColorsIncludedInQuery = permittedColors.some(
-            (permittedColor) => {
-              if (permittedColor === 'Colorless') {
-                // If the card is colorless, and we want colorless cards
-                // included, it'll have an empty array for its colors property
-                return cardColors.length === 0;
-              } else {
-                return cardColors.includes(permittedColor);
-              }
+            if (!matchesSearchText) {
+              return false;
             }
+          }
+
+          /**
+           * Check if the card passes the permitted colors
+           */
+
+          if (arePermittedColorsPresent) {
+            const manaCost = hasMultipleFaces
+              ? card.card_faces[0].mana_cost.concat(
+                  card.card_faces[1].mana_cost
+                )
+              : card.mana_cost;
+
+            const areAnyCardColorsIncludedInTheQuery = permittedColors.some(
+              (permittedColor) => {
+                if (permittedColor === 'Colorless') {
+                  const areAnyColorsPresent = colorsRegex.test(manaCost);
+
+                  return !areAnyColorsPresent;
+                } else {
+                  return manaCost.includes(permittedColor);
+                }
+              }
+            );
+
+            if (!areAnyCardColorsIncludedInTheQuery) {
+              return false;
+            }
+          }
+
+          /**
+           * If we've made it this far, the card did not fail any of the checks
+           */
+
+          return true;
+        } catch (error) {
+          warning(
+            false,
+            `
+📢 An error was encountered while filtering the cards:
+🔥    ${error}
+
+💡 To see the offending card, please visit:
+🌎    ${card.uri}
+`
           );
 
-          if (!someCardColorsIncludedInQuery) {
-            return false;
-          }
+          return false;
         }
-
-        // If we've made it this far, the card did not fail any of the checks
-        return true;
       })
     : cards;
 
