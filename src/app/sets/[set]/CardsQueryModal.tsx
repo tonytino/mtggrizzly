7307b37ce527b5fc8f2dcc2f114509bfda83fade
Anthony Hernandez <tony91139@gmail.com>
2023-12-29 21:09:53 -0700

    refactor(<CardsQueryModal />): Accommodate more query options

--- src/app/sets/[set]/CardsQueryModal.tsx
+++ src/app/sets/[set]/CardsQueryModal.tsx
@@ -1,3 +1,9 @@
+/**
+ * This file will be refactored as part of issue #68
+ *
+ * https://github.com/tonytino/mtggrizzly/issues/68
+ */
+
 'use client';
 
 import * as React from 'react';
@@ -43,10 +49,15 @@ function CardsQueryModal() {
 
   const isSearchTextPresent = Boolean(searchText);
 
+  /**
+   * Scroll to the top of the page whenever the queries change
+   */
   React.useEffect(
     () => scrollToTop,
     [permittedCardTypes, permittedColors, searchText]
   );
+
+  const onClose = React.useCallback(() => setIsOpen(false), []);
 
   return (
     <Dialog.Root
@@ -76,73 +87,64 @@ function CardsQueryModal() {
         <Dialog.Overlay
           aria-label={closeModalLabel}
           className='fixed bottom-0 left-0 right-0 top-0 z-20 h-full w-full cursor-pointer bg-slate-900 opacity-75'
-          onClick={() => setIsOpen(false)}
+          onClick={onClose}
         />
 
         <Dialog.Content className='fixed bottom-0 left-0 right-0 top-0 z-30 m-auto h-dvh w-screen overflow-y-auto bg-white p-4 text-sky-800 lg:h-3/4 lg:w-[800px] lg:rounded-md lg:p-8 xl:w-2/4 xl:min-w-[800px] dark:bg-slate-800 dark:text-slate-100'>
-          <div className='flex h-full w-full flex-col justify-between gap-8'>
-            <div className='flex flex-col gap-8'>
-              <div>
-                <Dialog.Title className='w-full text-2xl font-bold lg:text-center'>
-                  Search the Set
-                </Dialog.Title>
+          <div className='flex h-full w-full flex-col justify-between'>
+            <CardsQueryModalHeader onClose={onClose} />
 
-                <Dialog.Close asChild>
-                  <CloseQueryOptionsButton onClick={() => setIsOpen(false)} />
-                </Dialog.Close>
+            <div
+              className='flex flex-grow flex-col gap-8 overflow-y-scroll pb-2 pr-2'
+              id='query-controls'
+            >
+              <div className='relative flex w-full flex-col gap-2 text-slate-500 focus-within:text-sky-800 dark:text-slate-100 dark:focus-within:text-slate-100'>
+                <label
+                  className='font-bold'
+                  htmlFor='card-query-input'
+                >
+                  Search
+                </label>
 
-                <Dialog.Description className='py-8 text-lg'>
-                  Use the options below to search for matching cards
-                </Dialog.Description>
+                <input
+                  className='w-full rounded-lg border-2 border-slate-200 p-2 px-4 dark:border-transparent dark:text-sky-700'
+                  name='card-query-input'
+                  onChange={(event) => {
+                    setQueries((queries) => {
+                      return {
+                        ...queries,
+                        searchText: event.target.value,
+                      };
+                    });
+                  }}
+                  placeholder='Storm...'
+                  ref={searchTextInputRef}
+                  type='text'
+                  value={searchText}
+                />
 
-                <div className='relative flex min-w-full flex-col items-start gap-2 text-slate-500 focus-within:text-sky-800 md:min-w-[22rem] dark:text-slate-100 dark:focus-within:text-slate-100'>
-                  <label
-                    className='w-full font-bold'
-                    htmlFor='card-query-input'
-                  >
-                    Search
-                  </label>
-
-                  <input
-                    className='w-full rounded-lg border-2 border-slate-200 p-2 px-4 dark:border-transparent dark:text-sky-700'
-                    name='card-query-input'
-                    onChange={(event) => {
+                {isSearchTextPresent && (
+                  <button
+                    className='absolute bottom-2 right-4 rounded px-2 py-0.5 hover:bg-slate-100 dark:text-sky-700'
+                    onClick={() => {
                       setQueries((queries) => {
                         return {
                           ...queries,
-                          searchText: event.target.value,
+                          searchText: '',
                         };
                       });
+                      searchTextInputRef.current.focus();
                     }}
-                    placeholder='Storm...'
-                    ref={searchTextInputRef}
-                    type='text'
-                    value={searchText}
-                  />
-
-                  {isSearchTextPresent && (
-                    <button
-                      className='absolute bottom-2 right-4 rounded px-2 py-0.5 hover:bg-slate-100 dark:text-sky-700'
-                      onClick={() => {
-                        setQueries((queries) => {
-                          return {
-                            ...queries,
-                            searchText: '',
-                          };
-                        });
-                        searchTextInputRef.current.focus();
-                      }}
-                      type='button'
-                    >
-                      Clear
-                    </button>
-                  )}
-                </div>
+                    type='button'
+                  >
+                    Clear
+                  </button>
+                )}
               </div>
 
-              <div className='relative flex min-w-full flex-col items-start gap-2 text-slate-500 focus-within:text-sky-800 md:min-w-[22rem] dark:text-slate-100 dark:focus-within:text-slate-100'>
+              <div className='flex flex-col gap-2 text-slate-500 focus-within:text-sky-800 dark:text-slate-100 dark:focus-within:text-slate-100'>
                 <label
-                  className='w-full font-bold'
+                  className='font-bold'
                   htmlFor='selected-card-types'
                 >
                   Card Types
@@ -181,9 +183,9 @@ function CardsQueryModal() {
                 </ToggleGroup.Root>
               </div>
 
-              <div className='relative flex min-w-full flex-col items-start gap-2 text-slate-500 focus-within:text-sky-800 md:min-w-[22rem] dark:text-slate-100 dark:focus-within:text-slate-100'>
+              <div className='flex flex-col gap-2 text-slate-500 focus-within:text-sky-800 dark:text-slate-100 dark:focus-within:text-slate-100'>
                 <label
-                  className='w-full font-bold'
+                  className='font-bold'
                   htmlFor='selected-colors'
                 >
                   Colors
@@ -273,37 +275,112 @@ function CardsQueryModal() {
               </div>
             </div>
 
-            <div className='flex flex-col justify-between gap-4 md:flex-row-reverse'>
-              <button
-                aria-label={closeModalLabel}
-                className='w-[calc(100vw-2rem)] rounded-md bg-sky-700 py-4 text-center text-xl font-semibold text-slate-100 hover:bg-sky-600 lg:w-[var(--Set--width-desktop)] dark:bg-slate-200 dark:text-sky-800 dark:hover:bg-slate-100 dark:hover:text-sky-600'
-                onClick={() => setIsOpen(false)}
-                type='button'
-              >
-                Close
-              </button>
-
-              <button
-                aria-label='Reset the search options'
-                className='w-[calc(100vw-2rem)] rounded-md border-4 border-slate-500 py-2 text-center text-xl font-semibold text-slate-500 hover:border-red-500 hover:text-red-500 focus:border-red-500 focus:text-red-500 lg:w-[var(--Set--width-desktop)] landscape:mb-4 md:landscape:mb-0 dark:border-slate-400 dark:text-slate-400 dark:hover:border-red-500 dark:focus:border-red-500'
-                onClick={() => {
-                  if (
-                    window.confirm('Are you sure you want to reset everything?')
-                  ) {
-                    clearQueries();
-                  }
-                }}
-                type='button'
-              >
-                Reset
-              </button>
-            </div>
+            <CardsQueryModalFooter
+              onClear={() => clearQueries()}
+              onClose={onClose}
+            />
           </div>
         </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
   );
 }
+
+type CardsQueryModalHeaderType = {
+  /**
+   * Function that sets the query options modal to closed
+   */
+  onClose: () => void;
+};
+
+function CardsQueryModalHeader(props: CardsQueryModalHeaderType) {
+  return (
+    <div className='relative'>
+      <Dialog.Title className='w-full text-2xl font-bold lg:text-center'>
+        Search the Set
+      </Dialog.Title>
+
+      <Dialog.Close asChild>
+        <CloseQueryOptionsIcon onClick={props.onClose} />
+      </Dialog.Close>
+
+      <Dialog.Description className='py-4 text-sm md:py-6 md:text-base'>
+        Use the options below to search for matching cards
+      </Dialog.Description>
+    </div>
+  );
+}
+
+type CardsQueryModalFooterType = {
+  /**
+   * Function that sets the query options modal to closed
+   */
+  onClose: () => void;
+  /**
+   * Function that sets the query options modal to closed
+   */
+  onClear: () => void;
+};
+
+function CardsQueryModalFooter(props: CardsQueryModalFooterType) {
+  return (
+    <div className='flex flex-col justify-between gap-4 pt-4 md:flex-row-reverse md:gap-8 landscape:flex-row-reverse'>
+      <button
+        aria-label={closeModalLabel}
+        className='h-14 w-[calc(100vw-2rem)] rounded-md bg-sky-700 text-center text-xl font-semibold text-slate-100 hover:bg-sky-600 lg:w-[var(--Set--width-desktop)] dark:bg-slate-200 dark:text-sky-800 dark:hover:bg-slate-100 dark:hover:text-sky-600'
+        onClick={props.onClose}
+        type='button'
+      >
+        Close
+      </button>
+
+      <button
+        aria-label='Reset the search options'
+        className='h-14 w-[calc(100vw-2rem)] rounded-md border-4 border-slate-500 text-center text-xl font-semibold text-slate-500 hover:border-red-500 hover:text-red-500 focus:border-red-500 focus:text-red-500 lg:w-[var(--Set--width-desktop)] dark:border-slate-400 dark:text-slate-400 dark:hover:border-red-500 dark:focus:border-red-500'
+        onClick={() => {
+          if (window.confirm('Are you sure you want to reset everything?')) {
+            props.onClear();
+          }
+        }}
+        type='button'
+      >
+        Reset
+      </button>
+    </div>
+  );
+}
+
+type CloseQueryOptionsIconType = {
+  /**
+   * Function that sets the query options modal to closed
+   */
+  onClick: () => void;
+  /**
+   * The forwarded ref for [Radix](https://www.radix-ui.com/primitives/docs/guides/composition#your-component-must-forward-ref)
+   */
+  ref: React.ForwardedRef<HTMLButtonElement>;
+};
+
+const CloseQueryOptionsIcon = React.forwardRef(function CloseQueryOptionsIcon(
+  props: CloseQueryOptionsIconType,
+  forwardedRef
+) {
+  return (
+    <button
+      aria-label={closeModalLabel}
+      className='absolute right-0 top-0'
+      onClick={props.onClick}
+      ref={forwardedRef}
+      type='button'
+      {...props}
+    >
+      <Cross2Icon
+        height='32px'
+        width='32px'
+      />
+    </button>
+  );
+});
 
 type OpenQueryOptionsButtonType = {
   /**
@@ -337,39 +414,5 @@ const OpenQueryOptionsButton = React.forwardRef(function OpenQueryOptionsButton(
     </button>
   );
 });
-
-type CloseQueryOptionsButtonType = {
-  /**
-   * Function that sets the query options modal to closed
-   */
-  onClick: () => void;
-  /**
-   * The forwarded ref for [Radix](https://www.radix-ui.com/primitives/docs/guides/composition#your-component-must-forward-ref)
-   */
-  ref: React.ForwardedRef<HTMLButtonElement>;
-};
-
-const CloseQueryOptionsButton = React.forwardRef(
-  function CloseQueryOptionsButton(
-    props: CloseQueryOptionsButtonType,
-    forwardedRef
-  ) {
-    return (
-      <button
-        aria-label={closeModalLabel}
-        className='absolute right-4 top-4 lg:right-8 lg:top-8'
-        onClick={props.onClick}
-        ref={forwardedRef}
-        type='button'
-        {...props}
-      >
-        <Cross2Icon
-          height='32px'
-          width='32px'
-        />
-      </button>
-    );
-  }
-);
 
 export default CardsQueryModal;
