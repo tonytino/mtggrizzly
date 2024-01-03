5e8f57977e32d16b4ad75ffc2c34f0409fb55e2d
Anthony Hernandez <tony91139@gmail.com>
2024-01-02 20:11:30 -0700

    feat(<OpenCardsQueryModalButton />): New component

---
+++ src/app/sets/[set]/components/OpenCardsQueryModalButton.tsx
@@ -0,0 +1,77 @@
+'use client';
+
+import * as React from 'react';
+// https://www.radix-ui.com/primitives/docs/components/dialog
+import * as Dialog from '@radix-ui/react-dialog';
+// https://www.radix-ui.com/primitives/docs/components/toggle-group
+import * as Tooltip from '@radix-ui/react-tooltip';
+import { MixerHorizontalIcon } from '@radix-ui/react-icons';
+
+const openModalTooltipLabel = 'Search within the set';
+
+type OpenCardsQueryModalButtonType = {
+  /**
+   * Function that sets the status of `<CardsQueryModal />` to "open"
+   */
+  onClick: () => void;
+};
+
+/**
+ * Opens the `<CardsQueryModal />`
+ */
+export function OpenCardsQueryModalButton(
+  props: OpenCardsQueryModalButtonType
+) {
+  return (
+    <Tooltip.Root>
+      <Tooltip.Trigger asChild>
+        <Dialog.Trigger asChild>
+          <DialogTriggerButton onClick={props.onClick} />
+        </Dialog.Trigger>
+      </Tooltip.Trigger>
+
+      <Tooltip.Content
+        align='start'
+        alignOffset={22}
+        aria-label={openModalTooltipLabel}
+        className='mb-2 rounded bg-sky-800 p-2 text-sm text-slate-100 shadow-xl dark:bg-slate-50 dark:text-sky-800'
+        side='left'
+        sideOffset={12}
+      >
+        {openModalTooltipLabel}
+      </Tooltip.Content>
+    </Tooltip.Root>
+  );
+}
+
+type DialogTriggerButtonType = OpenCardsQueryModalButtonType & {
+  /**
+   * The forwarded ref
+   */
+  ref: React.ForwardedRef<HTMLButtonElement>;
+};
+
+/**
+ * Renders a button that supports forwarded refs for [Radix](https://www.radix-ui.com/primitives/docs/guides/composition#your-component-must-forward-ref)'s `<Dialog.Trigger />`
+ */
+const DialogTriggerButton = React.forwardRef(function DialogTriggerButton(
+  props: DialogTriggerButtonType,
+  forwardedRef
+) {
+  return (
+    <button
+      aria-label='Open the query options'
+      className='fixed bottom-8 right-8 h-20 w-20 rounded-full bg-sky-800 text-slate-100 shadow-xl dark:bg-slate-50 dark:text-sky-800'
+      onClick={props.onClick}
+      ref={forwardedRef}
+      type='button'
+      {...props}
+    >
+      <MixerHorizontalIcon
+        className='m-auto text-white dark:text-sky-800'
+        height='32px'
+        width='32px'
+      />
+    </button>
+  );
+});
---
+++ src/app/sets/[set]/components/index.ts
@@ -0,0 +1,1 @@
+export { OpenCardsQueryModalButton } from './OpenCardsQueryModalButton';
