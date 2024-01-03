614db9292bc21de7d57c165c946dc6ea2ec1f1a9
Anthony Hernandez <tony91139@gmail.com>
2024-01-03 10:54:57 -0700

    refactor(<CardsQueryModal />): Implement <CardsQueryModalFooter />

--- src/app/sets/[set]/CardsQueryModal.tsx
+++ src/app/sets/[set]/CardsQueryModal.tsx
@@ -14,7 +14,11 @@ import * as ToggleGroup from '@radix-ui/react-toggle-group';
 import CardsQueryContext from './CardsQueryContext';
 import { scrollToTop } from '@/root/utils';
 import type { CardType } from '@/types/card';
-import { CardsQueryModalHeader, OpenCardsQueryModalButton } from './components';
+import {
+  CardsQueryModalFooter,
+  CardsQueryModalHeader,
+  OpenCardsQueryModalButton,
+} from './components';
 
 const closeModalLabel = 'Close the search options interface';
 const cardTypes = [
@@ -262,6 +266,7 @@ function CardsQueryModal() {
             </div>
 
             <CardsQueryModalFooter
+              closeModalLabel={closeModalLabel}
               onClear={() => clearQueries()}
               onClose={onClose}
             />
@@ -269,45 +274,6 @@ function CardsQueryModal() {
         </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
-  );
-}
-
-type CardsQueryModalFooterType = {
-  /**
-   * Function that sets the query options modal to closed
-   */
-  onClose: () => void;
-  /**
-   * Function that sets the query options modal to closed
-   */
-  onClear: () => void;
-};
-
-function CardsQueryModalFooter(props: CardsQueryModalFooterType) {
-  return (
-    <div className='flex flex-col justify-between gap-4 pt-4 md:flex-row-reverse md:gap-8 landscape:flex-row-reverse'>
-      <button
-        aria-label={closeModalLabel}
-        className='h-14 w-[calc(100vw-2rem)] rounded-md bg-sky-700 text-center text-xl font-semibold text-slate-100 hover:bg-sky-600 lg:w-[var(--Set--width-desktop)] dark:bg-slate-200 dark:text-sky-800 dark:hover:bg-slate-100 dark:hover:text-sky-600'
-        onClick={props.onClose}
-        type='button'
-      >
-        Close
-      </button>
-
-      <button
-        aria-label='Reset the search options'
-        className='h-14 w-[calc(100vw-2rem)] rounded-md border-4 border-slate-500 text-center text-xl font-semibold text-slate-500 hover:border-red-500 hover:text-red-500 focus:border-red-500 focus:text-red-500 lg:w-[var(--Set--width-desktop)] dark:border-slate-400 dark:text-slate-400 dark:hover:border-red-500 dark:focus:border-red-500'
-        onClick={() => {
-          if (window.confirm('Are you sure you want to reset everything?')) {
-            props.onClear();
-          }
-        }}
-        type='button'
-      >
-        Reset
-      </button>
-    </div>
   );
 }
 
