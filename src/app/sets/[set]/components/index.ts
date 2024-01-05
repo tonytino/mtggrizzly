c507e63a32d6b74231b4757f74930fc61f9c1a50
Anthony Hernandez <tony91139@gmail.com>
2024-01-04 21:37:54 -0700

    feat(set page): Create <ColorsFilter />

--- src/app/sets/[set]/components/index.ts
+++ src/app/sets/[set]/components/index.ts
@@ -1,4 +1,5 @@
 export { CardTypesFilter } from './CardTypesFilter';
+export { ColorsFilter } from './ColorsFilter';
 export { CardsQueryModalFooter } from './CardsQueryModalFooter';
 export { CardsQueryModalHeader } from './CardsQueryModalHeader';
 export { OpenCardsQueryModalButton } from './OpenCardsQueryModalButton';
