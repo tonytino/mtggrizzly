1112143e493c0536f0144c3d53f76e8d943a7910
Anthony Hernandez <tony91139@gmail.com>
2024-01-04 21:31:36 -0700

    feat(set page): Create <CardTypesFilter />

--- src/app/sets/[set]/components/index.ts
+++ src/app/sets/[set]/components/index.ts
@@ -1,3 +1,4 @@
+export { CardTypesFilter } from './CardTypesFilter';
 export { CardsQueryModalFooter } from './CardsQueryModalFooter';
 export { CardsQueryModalHeader } from './CardsQueryModalHeader';
 export { OpenCardsQueryModalButton } from './OpenCardsQueryModalButton';
