fd0d13e04d0334bdc0e735fc72795d7d9a80c3be
Anthony Hernandez <tony91139@gmail.com>
2024-01-11 16:21:15 -0700

    chore(cypress config): Enable experimentalRunAllSpecs

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,8 +2,11 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    //   setupNodeEvents(on, config) {
-    //     // implement node event listeners here
-    //   },
+    // Presents the option to run all cypress specs at once
+    experimentalRunAllSpecs: true,
+
+    // Write specs by interacting with the browser!
+    // https://docs.cypress.io/guides/references/cypress-studio
+    // experimentalStudio: true,
   },
 });
