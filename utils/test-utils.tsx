6de1d80563c15cabd72ca34de1bdcd833e2b4150
Anthony Hernandez <tony91139@gmail.com>
2024-01-11 16:27:59 -0700

    chore(test-utils): Remove boilerplate

--- utils/test-utils.tsx
+++ utils/test-utils.tsx
@@ -12,20 +12,8 @@ import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
 import Providers from '@/src/app/Providers';
 
-// import { ThemeProvider } from 'my-ui-lib';
-// import { TranslationProvider } from 'my-i18n-lib';
-// import defaultStrings from 'i18n/en-x-default';
-
 const AllTheProviders = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <>
-      {/* <ThemeProvider theme='light'> */}
-      {/* <TranslationProvider messages={defaultStrings}> */}
-      <Providers>{children}</Providers>
-      {/* </TranslationProvider> */}
-      {/* </ThemeProvider> */}
-    </>
-  );
+  return <Providers>{children}</Providers>;
 };
 
 const renderWithProviders = (
