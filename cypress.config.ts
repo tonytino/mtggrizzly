8addc0d1d9877253df2490be95b669c34c5ffc21
Anthony Hernandez <tony91139@gmail.com>
2024-01-11 17:14:46 -0700

    chore(cypress config): Add baseUrl and link to docs

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,10 @@
+// https://docs.cypress.io/guides/references/configuration
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
+    baseUrl: 'http://localhost:3000',
+
     // Presents the option to run all cypress specs at once
     experimentalRunAllSpecs: true,
 
